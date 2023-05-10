3d37ab5cca15181bbcc674c2b8a557df03cabaae
a22ricni <a22ricni@student.his.se>
2023-05-10 09:38:04 +0200

    Added a recyclerViewItem class
    I added a new class with name RecyclerViewItem.

---
+++ app/src/main/java/com/example/end_project/RecyclerViewItem.java
@@ -0,0 +1,14 @@
+package com.example.end_project;
+
+public class RecyclerViewItem {
+
+    private String title;
+
+    public RecyclerViewItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
